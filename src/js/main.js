1b9845064207701f2fcaf4d37de7e0ba1a8a4d41
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2022-03-30 05:52:30 +0100

    fixed carousel issue

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,24 @@ const items = [
 const options = {
     activeItemPosition: 1,
     interval: 3000,
+    indicators: {
+        activeClasses: 'bg-indi-act',
+        inactiveClasses: 'bg-indi-def hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1')
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2')
+            },
+            {
+                position: 2,
+                el: document.getElementById('carousel-indicator-3')
+            },
+        ]
+    }
 }
 
 const carousel = new Carousel(items,options);
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,25 @@ module.exports = {
         grey3: "#3B3B3E",
         grey4: "#59566B",
         grey5: "#12141D",
+        grey6: "#29BB89",
         lightblue: "#258AFF",
         lightblue2: "#645CFE",
         lightblue3: "#6F55FF",
         lightblue4: "#5149C6",
         linkblue: "#3E3FD8",
         golden: "#FA4F58",
-        heart:"#FF523D",
+        rod: "#C40E0E",
+        heart: "#FF523D",
         lightgrey: "#7D7D7D",
         bglime: "#F8F9FC",
         carton: "#C4C4C4",
         ft: "#52525B",
-        fth:"#18181B",
-        line:"#DADADA"
+        fth: "#18181B",
+        line: "#DADADA",
+        line2:"#D8D8D8",
+        "indi-def":"#E7E7E7",
+        "indi-act":"#5C55CA"
+
       },
       fontFamily: {
         mon: ["Montserrat", "sans-serif"],
